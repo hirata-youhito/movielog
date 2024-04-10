2d17957fb7f7d8159de1f0e4f9b6d0210a50690a
unknown <akito_hirata@mforce.co.jp>
2024-04-11 00:15:47 +0900

    20240410

--- lib/component/searchMovie/movieList.tsx
+++ lib/component/searchMovie/movieList.tsx
@@ -1,8 +1,10 @@
+'use client'
 
 import React,{FC} from "react";
 
 import Image from 'next/image';
 import { StaticImageData } from "next/image"
+import {useState} from "react"
 
 import {Images} from "../../type"
 
@@ -31,35 +33,7 @@ export default function MovieList()  {
 
     //const movieId = "670292";
 
-    
-
-
-    return (
-        <div className="grid grid-cols-3 gap-4 content-normal">
-            {infoArray.map((info) => {
-                return(
-                    <div className="w-full h-full">
-                        <Link
-                            className=""
-                            href = {`http://localhost:3000/movieInfo?movieId=${info.id}`}
-                            key = {info.key}
-                        >
-                            <Image
-                                className="w-full h-44"
-                                src={`https://image.tmdb.org/t/p/w500${getImage(info.id)}`}
-                                key={info.key}
-                                alt="movieImage"
-                                fill={true}
-                                //width={200}
-                                //height={200}
-                            />
-                        </Link>
-                        <div className="text-center text-blue-600">{info.title}</div>
-                    </div>
-                )
-            })}
-        </div>
-    );
+    //const [imageUrl,setimageUrl] = useState< string | null >(null)
 
     async function getImage(movieId:string) {
         const imageHeaders = new Headers();
@@ -74,12 +48,46 @@ export default function MovieList()  {
             }
         )
         const image:Images = await response.json();
+        //console.log(image)
         console.log(image.backdrops[0].file_path)
-        const imageFile_path = image.backdrops[0].file_path
+        console.log("loop")
         
-        return{
-            imageFile_path
-        }
+        return(
+            image.backdrops[0].file_path
+        )
     }
+
+
+    return (
+        <div className="grid grid-cols-3 gap-4 content-normal">
+            {infoArray.map((info) => {
+                const imageUrl = `https://image.tmdb.org/t/p/w500${getImage(info.id)}`
+                //const imageUrl = async () => {
+                //    `https://image.tmdb.org/t/p/w500${getImage(info.id)}`
+                //}
+                console.log(imageUrl)
+                return(
+                    <div key = {info.key} className="w-full h-full">
+                        <Link
+                            className=""
+                            href = {`http://localhost:3000/movieInfo?movieId=${info.id}`}
+                            key = {info.key}
+                        >
+                            <Image
+                                className="w-full h-44"
+                                src={imageUrl}
+                                key={info.key}
+                                alt="movieImage"
+                                fill={true}
+                                //width={200}
+                                //height={200}
+                            />
+                        </Link>
+                        <div className="text-center text-blue-600">{info.title}</div>
+                    </div>
+                )
+            })}
+        </div>
+    );
 };
 
